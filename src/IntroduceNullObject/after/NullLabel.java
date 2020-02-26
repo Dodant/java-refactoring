ed5db6963d7e78e15607c1b5c4ec698b1a31de45
Dodanto <ohho0728@naver.com>
2020-02-26 14:40:32 +0900

    Fin : Chap 4

---
+++ src/IntroduceNullObject/after/NullLabel.java
@@ -0,0 +1,12 @@
+package IntroduceNullObject.after;
+
+public class NullLabel extends Label {
+    public NullLabel() {
+        super("(none)");
+    }
+    @Override public void display() {
+    }
+    @Override public boolean isNull() {
+        return true;
+    }
+}
---
+++ src/IntroduceNullObject/step2/NullLabel.java
@@ -0,0 +1,10 @@
+package IntroduceNullObject.step2;
+
+public class NullLabel extends Label {
+    public NullLabel() {
+        super("(none)");
+    }
+    @Override public boolean isNull() {
+        return true;
+    }
+}
