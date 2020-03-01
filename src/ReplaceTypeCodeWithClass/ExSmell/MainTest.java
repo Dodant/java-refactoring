21953e29f6416f3b089ce4f42ea025420bd8f61d
Dodanto <ohho0728@naver.com>
2020-03-01 20:03:43 +0900

    Fin : Chap 7

---
+++ src/ReplaceTypeCodeWithClass/ExSmell/MainTest.java
@@ -0,0 +1,21 @@
+package ReplaceTypeCodeWithClass.ExSmell;
+
+import static org.junit.Assert.assertEquals;
+import junit.framework.JUnit4TestAdapter;
+import org.junit.*;
+import com.hyuki.refbook.StandardOutputTest;
+
+public class MainTest extends StandardOutputTest {
+    @Test public void testMain() {
+        Main.main(new String[0]);
+        String expected = getExpectedOutput(
+            "[ LunchSet: A, JUICE ]",
+            "[ LunchSet: B, COFFEE ]");
+        String actual = getActualOutput();
+        assertEquals(expected, actual);
+    }
+
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(MainTest.class);
+    }
+}
